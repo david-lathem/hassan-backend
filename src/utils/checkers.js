7701d0f140dcd8f33c11c80c7c4c62eb9aa579fc
bilal-the-dev <bilalfarooq6001@gmail.com>
2025-12-13 13:56:23 +0500

    change to multi

--- src/utils/checkers.js
+++ src/utils/checkers.js
@@ -4,7 +4,7 @@ import { botType, botTypeArray } from "./constants.js";
 export const isCorrectBotType = (req) => {
   if (!botTypeArray.includes(req.query.tokenType))
     throw new AppError(
-      `tokenType must be either "${botType.NORMAL_BOT}" or "${botType.SELF_BOT}"`,
+      `tokenType (in query params) must be either "${botType.NORMAL_BOT}" or "${botType.SELF_BOT}"`,
       400
     );
 };
