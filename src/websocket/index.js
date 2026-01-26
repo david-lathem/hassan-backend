8a5ab908f321656c3378e61394557ab19beedbfa
bilal-the-dev <bilalfarooq6001@gmail.com>
2026-01-27 00:24:58 +0500

    s

--- src/websocket/index.js
+++ src/websocket/index.js
@@ -8,8 +8,6 @@ wss.on("connection", function connection(ws) {
   ws.on("message", function message(data) {
     console.log("received: %s", data);
   });
-
-  ws.send("something");
 });
 
 export default wss;
