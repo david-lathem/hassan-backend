3b8f2343088eb2306fcbb86fbafce906f4393cbf
bilal-the-dev <bilalfarooq6001@gmail.com>
2026-02-10 12:45:54 +0500

    s

--- src/discord/client.js
+++ src/discord/client.js
@@ -1,5 +1,9 @@
 import { Client, WebhookClient } from "discord.js-selfbot-v13";
-import { CHANNEL_IDS, CHANNEL_IDS_MAP } from "../utils/constants.js";
+import {
+  FORWARD_CHANNEL_DATA,
+  FOWARD_CHANNEL_IDS,
+  WEB_CHANNEL_IDS,
+} from "../utils/constants.js";
 import Messages from "../models/messages.js";
 import wss from "../websocket/index.js";
 import { WebSocket } from "ws";
@@ -13,15 +17,11 @@ client.on("ready", async () => {
 
 client.on("messageCreate", async (message) => {
   try {
-    if (!CHANNEL_IDS.includes(message.channelId)) return;
+    if (!WEB_CHANNEL_IDS.includes(message.channelId)) return;
 
     const { content, embeds, attachments, author, channel } = message;
 
-    const data = { content, files: [...attachments.values()], embeds };
-
-    const webhook = new WebhookClient({ url: CHANNEL_IDS_MAP[channel.id] });
-
-    await webhook.send(data);
+    // const data = { content, files: [...attachments.values()], embeds };
 
     const f = await Messages.create({
       channelId: channel.id,
@@ -40,6 +40,49 @@ client.on("messageCreate", async (message) => {
 
       client.send(JSON.stringify(f));
     });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+client.on("messageCreate", async (message) => {
+  try {
+    if (!FOWARD_CHANNEL_IDS.includes(message.channelId)) return;
+
+    const { content, embeds, attachments, channel } = message;
+
+    const configData = FORWARD_CHANNEL_DATA[channel.id];
+    if (configData.tc) {
+      embeds.forEach((e) => {
+        e.title?.replace("TC", "");
+        e.description?.replace("TC", "");
+        e.footer?.text?.replace("TC", "");
+      });
+    }
+
+    if (
+      configData.excludes?.some((word) =>
+        content.toLowerCase().includes(word.toLowerCase()),
+      )
+    )
+      return;
+
+    if (configData.includes?.length) {
+      if (
+        !configData.includes.some((word) =>
+          content.toLowerCase().includes(word.toLowerCase()),
+        )
+      )
+        return;
+    }
+
+    const data = { content, files: [...attachments.values()], embeds };
+
+    const webhook = new WebhookClient({
+      url: configData.WEBHOOK_URL,
+    });
+
+    await webhook.send(data);
   } catch (error) {
     console.error(error);
   }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,8 @@
 import config from "./../../config.json" with { type: "json" };
 
-export const CHANNEL_IDS = Object.keys(config);
-export const CHANNEL_IDS_MAP = config;
+export const WEB_CHANNEL_IDS = process.env.WEB_CHANNEL_IDS.split(",");
+export const FOWARD_CHANNEL_IDS = Object.keys(config);
+export const FORWARD_CHANNEL_DATA = config;
 
 export const COMMAND_MAP = {
   B: "Bought",
