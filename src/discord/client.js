f14ca08e777a99914921e41e25d281f32aa6fcab
bilal-the-dev <bilalfarooq6001@gmail.com>
2026-02-20 16:40:52 +0500

    s

--- src/discord/client.js
+++ src/discord/client.js
@@ -66,7 +66,7 @@ client.on("messageCreate", async (message) => {
 
     console.log(message.content);
 
-    let content = message.content ?? "No content";
+    let content = message.content || "No content";
 
     content = content.replace(/<@&\d+>/g, "");
 
