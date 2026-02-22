e3ab8d625cc3c7e0a13341354fe7bcc96ff97892
bilal-the-dev <bilalfarooq6001@gmail.com>
2026-02-22 11:26:17 +0500

    s

--- src/controllers/channelController.js
+++ src/controllers/channelController.js
@@ -2,9 +2,11 @@ import { FORWARD_CHANNEL_DATA } from "../utils/constants.js";
 import { sendResponse } from "../utils/sendResponse.js";
 
 export const sendChannelData = async (req, res) => {
-  const channelData = FORWARD_CHANNEL_DATA.map((d) => {
-    return { ...d, WEBHOOK_URL: undefined };
-  });
+  const channelData = Object.values(FORWARD_CHANNEL_DATA)
+    .filter((d) => !d.discord)
+    .map((d) => {
+      return { ...d, WEBHOOK_URL: undefined };
+    });
 
   sendResponse(req, res, channelData);
 };
