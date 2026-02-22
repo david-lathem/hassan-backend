344276463aca8c0c1253a35d597d78bb1cb726a5
bilal-the-dev <bilalfarooq6001@gmail.com>
2026-02-22 11:29:53 +0500

    s

--- src/controllers/channelController.js
+++ src/controllers/channelController.js
@@ -5,7 +5,12 @@ export const sendChannelData = async (req, res) => {
   const channelData = Object.entries(FORWARD_CHANNEL_DATA)
     .filter((d) => !d[1].discord)
     .map((d) => {
-      return { name: d[1].name, alternate: d[1].alternate, channelId: d[0] };
+      return {
+        name: d[1].name,
+        alternate: d[1].alternate,
+        tc: d[1].tc,
+        channelId: d[0],
+      };
     });
 
   sendResponse(req, res, channelData);
